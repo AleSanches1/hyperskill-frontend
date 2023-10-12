1e84d0d85ea473c488660b7e2a906f1890130de9
AleSanches1 <130606847+ale-sanches@users.noreply.github.com>
2023-10-12 19:16:02 +0400

    stage2/5

--- simple-chatty-bot/main.js
+++ simple-chatty-bot/main.js
@@ -1,6 +1,13 @@
- let bot_name = "Aleks";
+const input = require('sync-input');
+
+let yourName;
+let bot_name = "Aleks";
 let birth_year = 2023;
 
 console.log(`Hello! My name is ${bot_name}.
-I was created in ${birth_year}.`);
+I was created in ${birth_year}.
+Please, remind me your name.`);
 
+yourName = input();
+
+console.log(`What a great name you have, ${yourName}!`);
