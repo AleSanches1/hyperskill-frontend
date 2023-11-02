547db6a88ab1616ee41a4eeacd8d6abe197fe64c
AleSanches1 <130606847+ale-sanches@users.noreply.github.com>
2023-11-02 22:15:27 +0400

    coffee-machine: stage 3/6

--- coffee-machine/main.js
+++ coffee-machine/main.js
@@ -1,21 +1,32 @@
-const input = require('sync-input')
+const input = require('sync-input');
 
-let number_of_cups = Number(input('Write how many cups of coffee you will need:'));
-const water_in_one_cup = 200;
-const milk_in_one_cup = 50;
-const beans_in_one_cup = 15;
+const waterInOneCup = 200;
+const milkInOneCup = 50;
+const beansInOneCup = 15;
 
+const availableWater = Number(input('Write how many ml of water the coffee machine has:'));
+const availableMilk = Number(input('Write how many ml of milk the coffee machine has:'));
+const availableBeans = Number(input('Write how many grams of coffee beans the coffee machine has:'));
+const numberOfCups = Number(input('Write how many cups of coffee you will need:'));
 
-function calcIngredients(amount){
-    let amount_of_water = amount * water_in_one_cup;
-    let amount_of_milk = amount* milk_in_one_cup;
-    let amount_of_beans = amount * beans_in_one_cup;
-    console.log(`For ${number_of_cups} of coffee you will need:
-    ${amount_of_water} ml of water
-    ${amount_of_milk} ml of milk
-    ${amount_of_beans} g of coffee beans`);
+// Функция для вычисления необходимого количества ингредиентов для заданного количества чашек
+function calcIngredients(water, milk, beans, cups) {
+    const amountOfWater = cups * waterInOneCup;
+    const amountOfMilk = cups * milkInOneCup;
+    const amountOfBeans = cups * beansInOneCup;
+
+    return [amountOfWater, amountOfMilk, amountOfBeans];
 }
 
-calcIngredients(Number(number_of_cups));
+const [neededWater, neededMilk, neededBeans] = calcIngredients(availableWater, availableMilk, availableBeans, numberOfCups);
 
-
+const maxCups = Math.floor(Math.min(availableWater / waterInOneCup, availableMilk / milkInOneCup, availableBeans / beansInOneCup));
+if (maxCups >= numberOfCups) {
+    if (maxCups > numberOfCups) {
+        console.log(`Yes, I can make that amount of coffee (and even ${maxCups - numberOfCups} more than that)`);
+    } else {
+        console.log("Yes, I can make that amount of coffee");
+    }
+} else {
+    console.log(`No, I can make only ${maxCups} cups of coffee`);
+}
