9effe2489539750f0d7d7b988be4b20b044fdbcb
AleSanches1 <130606847+ale-sanches@users.noreply.github.com>
2023-11-17 13:35:38 +0400

    coffee-machine: stage 4/6

--- coffee-machine/main.js
+++ coffee-machine/main.js
@@ -1,32 +1,71 @@
 const input = require('sync-input');
 
-const waterInOneCup = 200;
-const milkInOneCup = 50;
-const beansInOneCup = 15;
-
-const availableWater = Number(input('Write how many ml of water the coffee machine has:'));
-const availableMilk = Number(input('Write how many ml of milk the coffee machine has:'));
-const availableBeans = Number(input('Write how many grams of coffee beans the coffee machine has:'));
-const numberOfCups = Number(input('Write how many cups of coffee you will need:'));
-
-// Функция для вычисления необходимого количества ингредиентов для заданного количества чашек
-function calcIngredients(water, milk, beans, cups) {
-    const amountOfWater = cups * waterInOneCup;
-    const amountOfMilk = cups * milkInOneCup;
-    const amountOfBeans = cups * beansInOneCup;
-
-    return [amountOfWater, amountOfMilk, amountOfBeans];
+let machineState = {
+    water: 400,
+    milk: 540,
+    beans: 120,
+    cups: 9,
+    money: 550,
 }
 
-const [neededWater, neededMilk, neededBeans] = calcIngredients(availableWater, availableMilk, availableBeans, numberOfCups);
 
-const maxCups = Math.floor(Math.min(availableWater / waterInOneCup, availableMilk / milkInOneCup, availableBeans / beansInOneCup));
-if (maxCups >= numberOfCups) {
-    if (maxCups > numberOfCups) {
-        console.log(`Yes, I can make that amount of coffee (and even ${maxCups - numberOfCups} more than that)`);
-    } else {
-        console.log("Yes, I can make that amount of coffee");
+function showMachineState() {
+    console.log(`The coffee machine has:
+${machineState.water} ml of water
+${machineState.milk} ml of milk
+${machineState.beans} g of coffee beans
+${machineState.cups} disposable cups
+$${machineState.money} of money`)
+}
+function makeCoffee(waterNeeded, milkNeeded, beansNeeded, moneyEarned) {
+    machineState.water -= waterNeeded;
+    machineState.milk -= milkNeeded;
+    machineState.beans -= beansNeeded;
+    machineState.money += moneyEarned;
+    machineState.cups -= 1;
+
+}
+
+function makeEspresso() {
+    makeCoffee(250, 0, 16, 4);
+}
+
+function makeLatte() {
+    makeCoffee(350, 75, 20, 7);
+}
+
+function makeCapuccino() {
+    makeCoffee(200, 100, 12, 6);
+}
+
+function fillTheMachine() {
+    machineState.water += Number(input('Write how many ml of water you want to add: '));
+    machineState.milk += Number(input('Write how many ml of milk you want to add: '));
+    machineState.beans += Number(input('Write how many grams of coffee beans you want to add: '));
+    machineState.cups += Number(input('Write how many disposable cups you want to add: '));
+}
+
+while (true) {
+    showMachineState();
+
+    const choice = input('Write action (buy, fill, take):');
+    if (choice === 'buy') {
+        const typeOfCoffee = input('What do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino:');
+        switch (typeOfCoffee) {
+            case '1':
+                makeEspresso();
+                break;
+            case '2':
+                makeLatte();
+                break;
+            case '3':
+                makeCapuccino();
+                break;
+        }
+    } else if (choice === 'fill') {
+        fillTheMachine();
+    } else if (choice === 'take') {
+        console.log(`I gave you $${machineState.money}`);
+        machineState.money = 0;
     }
-} else {
-    console.log(`No, I can make only ${maxCups} cups of coffee`);
 }
