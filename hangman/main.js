7282c0eebc418bb5aee93765fe564213c0e04fe2
AleSanches1 <130606847+ale-sanches@users.noreply.github.com>
2024-01-04 20:15:15 +0300

    hangman: step 3
    Players have exactly eight attempts to guess the word by entering letters one by one. Asking for a letter, print: Input a letter:. If a player uncovered all the letters, but some attempts are still left, the program must continue to ask for input until all the tries are exhausted;
    
    If the letter doesn't appear in the word, the computer takes one try away – even if a user has already suggested this letter – and prints That letter doesn't appear in the word.;
    
    If the letter does appear in the word but a user has already suggested this letter and it's open, no need to print any messages;
    
    If all attempts are exhausted, the game ends; the program shows a final message (Thanks for playing!). Otherwise, players can continue to input letters;

--- hangman/main.js
+++ hangman/main.js
@@ -3,23 +3,84 @@
 // You will need this in the following stages
 const input = require('sync-input')
 
+// Независимые от текущей игры
 let listOfWords = ["python", "java", "swift", "javascript"];
-let getRandomWord = () => Math.floor(Math.random() * listOfWords.length);
-let selectedWord = listOfWords[getRandomWord()];
+let getRandomIndex = () => Math.floor(Math.random() * listOfWords.length);
+const prompt = "\nInput a letter:"
 
-function hideAPartOfWord(word) {
-    return word.slice(0, 3) + word.slice(3).replace(/./g, '-');
+// Текущая игра:
+let currentWordLetters;
+let guessedWordLetters;
+let userGuessing;
+let userAttempts = 8;
+let guessedIndices;
+
+
+function prepareLetterArray() {
+    let word = listOfWords[getRandomIndex()];
+    currentWordLetters = word.split("");
 }
 
-
-console.log(`H A N G M A N`);
-
-let userInput = input(`Guess the word: ${hideAPartOfWord(selectedWord)}`);
-
-
-
-if (userInput === selectedWord) {
-    console.log("You survived!")
-} else {
-    console.log("You lost!")
+function makeGuessingLettersArray() {
+    guessedWordLetters = new Array(currentWordLetters.length);
 }
+
+function collectGuessedLetters() {
+    guessedIndices = [];
+
+    for (let i = 0; i < currentWordLetters.length; i++) {
+        if (currentWordLetters[i] === userGuessing) {
+            guessedIndices.push(i);
+        }
+    }
+
+    guessedIndices.forEach(index => {
+        guessedWordLetters.splice(index, 1, userGuessing);
+    });
+
+    return guessedWordLetters;
+}
+
+function collectString() {
+    let result = '';
+    for (let i = 0; i < guessedWordLetters.length; i++) {
+        if (guessedWordLetters[i] === undefined) {
+            result += "-";
+        } else {
+            result += guessedWordLetters[i];
+        }
+    }
+    return result;
+}
+
+function reduceAttempts(attempt) {
+    --userAttempts;
+}
+
+function showMessage() {
+    let messagingCondition = currentWordLetters.includes(userGuessing);
+    if (!messagingCondition) {
+        console.log("That letter doesn't appear in the word.");
+    }
+}
+
+//Run
+function runGame() {
+    console.log("H A N G M A N\n");
+    prepareLetterArray();
+    makeGuessingLettersArray();
+    do {
+        let s = collectString();
+        userGuessing = input(s + prompt);
+        collectGuessedLetters();
+        showMessage();
+        reduceAttempts(userAttempts);
+    }
+    while (userAttempts > 0);
+    if (userAttempts === 0) {
+        console.log('Thanks for playing!');
+    }
+}
+
+runGame();
+
