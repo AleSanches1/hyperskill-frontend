33f2dec4cf290c7e4ae824d9abf7643676e2a43e
AleSanches1 <130606847+ale-sanches@users.noreply.github.com>
2024-01-31 12:21:25 +0300

    carnival-gift-shop: stage 3
    menu items added

--- carnival-gift-shop/main.js
+++ carnival-gift-shop/main.js
@@ -1,5 +1,8 @@
+const input = require('sync-input');
+
 
 const greeting = "WELCOME TO THE CARNIVAL GIFT SHOP!\nHello friend! Thank you for visiting the carnival!"
+let availableTickets = 100;
 
 function Item(name, price, id) {
     this.name = name;
@@ -7,9 +10,9 @@ function Item(name, price, id) {
     this.id = id;
 }
 
-const giftList = [new Item("Teddy Bear", 10, 1),
+let giftList = [new Item("Teddy Bear", 10, 1),
     new Item("Big Red Ball", 5, 2),
-    new Item("Huge Bear", 50, 3),
+    new Item("Huge Bear", 50,3 ),
     new Item("Candy", 8, 4),
     new Item("Stuffed Tiger", 15, 5),
     new Item("Stuffed Dragon", 30, 6),
@@ -19,20 +22,60 @@ const giftList = [new Item("Teddy Bear", 10, 1),
     new Item("Scary Mask", 75, 10),
 ];
 
-
-function printGreetingMessage() {
-    console.log(greeting);
-    console.log("Here's the list of gifts:\n");
-}
-
 function printGiftList() {
+    console.log("Here's the list of gifts:\n");
     giftList.forEach(function (value) {
         console.log(`${value.id}- ${value.name}, Cost: ${value.price} tickets`);
     })
 }
 
+
+function printGreetingMessage() {
+    console.log(greeting);
+    printGiftList();
+}
+
+
+function buyAGift() {
+    let choice = input("Enter the number of the gift you want to get:");
+    let giftName = giftList[choice - 1].name;
+    let gifPrice = giftList[choice - 1].price;
+    console.log(`Here you go, one ${giftName}!`);
+    availableTickets -= gifPrice;
+    console.log("Total tickets: " + availableTickets);
+}
+
+function addTickets (){
+    let ticketAmount = input("Enter the ticket amount:")
+    let newTicketAmount =  availableTickets += Number(ticketAmount);
+    console.log("Total tickets: " + newTicketAmount);
+}
+function checkTickets() {
+    console.log("Total tickets: " + availableTickets);
+}
+
+
+
 printGreetingMessage();
-printGiftList();
 
+function chooseMenuItem(){
+let choice = input("\nWhat do you want to do?\n1-Buy a gift 2-Add tickets 3-Check tickets 4-Show gifts\n ");
+switch (choice) {
+    case "1":
+        buyAGift();
+        break
+    case "2":
+        addTickets();
+        break
+    case "3":
+        checkTickets();
+        break
+    case "4":
+        printGiftList();
+        break
 
+}
+console.log("Have a nice day!")
+}
 
+chooseMenuItem();
