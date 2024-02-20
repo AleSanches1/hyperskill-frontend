83026916d4a5be34e5aaeffdcf1b413e21ca411a
AleSanches1 <130606847+ale-sanches@users.noreply.github.com>
2024-02-20 17:21:56 +0300

    url-shortener: stage 1.1

--- url-shortener/main.js
+++ url-shortener/main.js
@@ -4,37 +4,43 @@ const enteredUrl = document.querySelector("input");
 const button = document.querySelector("button")
 const orderedList = document.querySelector('ol');
 
-function isURLValid(url){
+function isURLValid(url) {
     let regex = /^(http(s)?:\/\/)?([\w-]+\.)+[\w-]+(\/[\w-.\/?%:#&=]*)?$/;
     return regex.test(url);
 }
-// function generateRandomString(length) {
-//     let result = " ";
-//     let characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-//     let charactersLength = characters.length;
-//     for(let i = 0; i <= length; i++){
-//         result += characters.charAt(Math.floor(Math.random() * charactersLength))
-//     }
-//     return result;
-// }
-// function shortUrl(url) {
-//     let newUrl = "localhost/" + generateRandomString(5);
-//
-// }
+
+function generateRandomString(length) {
+    let result = "";
+    let characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+    let charactersLength = characters.length;
+    for (let i = 0; i < length; i++) {
+        result += characters.charAt(Math.floor(Math.random() * charactersLength))
+    }
+    return result;
+}
+
+function shortUrl() {
+    return "localhost/" + generateRandomString(5);
+}
 
 
-button.addEventListener('click', function (){
+button.addEventListener('click', function () {
     const message = document.querySelector(".message");
-    if (isURLValid(enteredUrl.value)){
-        if(!message.classList.contains("hidden")){
+    if (isURLValid(enteredUrl.value)) {
+        if (!message.classList.contains("hidden")) {
             message.classList.add("hidden");
-            //нужно показывать новую короткую ссылку, старую длинную, но короткая с адресом длинной
         }
         const ol = document.querySelector('ol');
         let li = document.createElement("li");
-        li.innerHTML = enteredUrl.value;
         ol.append(li);
-    }else if(!isURLValid(enteredUrl.value)){
+        let link = document.createElement("a");
+        link.href = enteredUrl.value;
+        link.textContent = shortUrl();
+        link.target = "_blank";
+        li.append(link);
+        li.innerHTML += " - " + enteredUrl.value;
+
+    } else if (!isURLValid(enteredUrl.value)) {
         // let message = document.createElement("p");
         //     message.innerHTML = "Please enter a valid url";
         //     button.after(message);
