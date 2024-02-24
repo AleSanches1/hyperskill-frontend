0156b92836d30de9e94f8e023cf0151d21ca3123
ale-sanches <130606847+ale-sanches@users.noreply.github.com>
2024-02-24 15:56:46 +0300

    "Coffee machine" project completed
    Write a program that puts some basic information on the screen. Let the machine describe what it takes to make a cup of coffee!
    The machine should calculate how many ingredients it needs to make the required amount of coffee.
    Program the machine to calculate how many cups it can make based on the number of set ingredients.
    Teach your virtual coffee machine to perform three basic actions: collect the money, replenish the supplies, and serve the coffee.
    Program the machine to display how many supplies it has on the screen. Work on the main loop: now, the menu keeps updating until users choose to exit.

--- coffee-machine/main.js
+++ coffee-machine/main.js
@@ -1,18 +1,151 @@
-const input = require('sync-input')
+const input = require('sync-input');
+const COFFEE_TYPES = {
+    1: {
+        name: 'Espresso',
+        water: 250,
+        milk: 0,
+        beans: 16,
+        money: 4,
+        cups: 1,
+    },
+    2: {
+        name: 'Latte',
+        water: 350,
+        milk: 75,
+        beans: 20,
+        money: 7,
+        cups: 1,
+    },
+    3: {
+        name: 'Cappuccino',
+        water: 200,
+        milk: 100,
+        beans: 12,
+        money: 6,
+        cups: 1,
+    },
+    4: {
+        name: 'Americano',
+        water: 150,
+        milk: 0,
+        beans: 18,
+        money: 5,
+        cups: 1,
+    },
+};
 
-let number_of_cups = Number(input('Write how many cups of coffee you will need:'));
-const water_in_one_cup = 200;
-const milk_in_one_cup = 50;
-const beans_in_one_cup = 15;
-
-function calcIngredients(amount){
-    let amount_of_water = amount * water_in_one_cup;
-    let amount_of_milk = amount* milk_in_one_cup;
-    let amount_of_beans = amount * beans_in_one_cup;
-    console.log(`For ${number_of_cups} of coffee you will need:
-    ${amount_of_water} ml of water
-    ${amount_of_milk} ml of milk
-    ${amount_of_beans} g of coffee beans`);
+let machineState = {
+    water: 400,
+    milk: 540,
+    beans: 120,
+    cups: 9,
+    money: 550,
 }
 
-calcIngredients(Number(number_of_cups));
+function showMachineState() {
+    console.log(`The coffee machine has:
+${machineState.water} ml of water
+${machineState.milk} ml of milk
+${machineState.beans} g of coffee beans
+${machineState.cups} disposable cups
+$${machineState.money} of money`);
+}
+
+function checkAvailability(coffeeType) {
+    if (machineState.water < coffeeType.water) {
+        console.log('Sorry, not enough water!');
+        return false;
+    } else if (machineState.milk < coffeeType.milk) {
+        console.log('Sorry, not enough milk!');
+        return false;
+    } else if (machineState.beans < coffeeType.beans) {
+        console.log('Sorry, not enough coffee beans!');
+        return false;
+    } else if (machineState.cups < coffeeType.cups) {
+        console.log('Sorry, not enough coffee cups!');
+        return false;
+    } else {
+        console.log('I have enough resources, making you a coffee!');
+        return true;
+    }
+}
+
+function makeCoffee(coffeeType, typeOfCoffee) {
+    if (checkAvailability(coffeeType)) {
+        machineState.water -= coffeeType.water;
+        machineState.milk -= coffeeType.milk;
+        machineState.beans -= coffeeType.beans;
+        machineState.money += coffeeType.money;
+        machineState.cups -= coffeeType.cups;
+
+        // Check if the coffee type is special before printing the special message
+        if (typeOfCoffee === 'random') {
+            console.log(`${coffeeType.name} is ready! Your special price is ${coffeeType.money}!`);
+        } else {
+            console.log(`${coffeeType.name} is ready!`);
+        }
+    }
+}
+
+function buyCoffee() {
+    const typeOfCoffee = input('What do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino, 4 - americano, random - try random coffee with special price, back - to main menu:');
+    const coffeeType = COFFEE_TYPES[typeOfCoffee];
+
+    if (typeOfCoffee === 'back') {
+        return;
+    }
+    if (typeOfCoffee === 'random') {
+        const specialOffer = getSpecialOffer();
+        makeCoffee(specialOffer, typeOfCoffee);
+    } else if (coffeeType) {
+        makeCoffee(coffeeType, typeOfCoffee);
+    } else {
+        console.log('Invalid coffee type. Please choose 1, 2, 3, special, or back.');
+    }
+}
+
+
+
+function getSpecialOffer() {
+    const randomCoffeeIndex = Math.floor(Math.random() * Object.values(COFFEE_TYPES).length);
+    const randomCoffee = Object.values(COFFEE_TYPES)[randomCoffeeIndex];
+
+    const updatedCoffee = { ...randomCoffee, money: randomCoffee.money - 2 };
+    return updatedCoffee;
+}
+
+function fillTheMachine() {
+    machineState.water += Number(input('Write how many ml of water you want to add: '));
+    machineState.milk += Number(input('Write how many ml of milk you want to add: '));
+    machineState.beans += Number(input('Write how many grams of coffee beans you want to add: '));
+    machineState.cups += Number(input('Write how many disposable cups you want to add: '));
+}
+
+function takeAction() {
+    console.log(`I gave you $${machineState.money}`);
+    machineState.money = 0;
+}
+
+while (true) {
+    const choice = input('Write action (buy, fill, take, remaining, exit):');
+
+    switch (choice) {
+        case 'buy':
+            buyCoffee();
+            break;
+        case 'fill':
+            fillTheMachine();
+            break;
+        case 'take':
+            takeAction();
+            break;
+        case 'remaining':
+            showMachineState();
+            break;
+        case 'exit':
+            process.exit();
+        default:
+            console.log('Invalid action. Please choose buy, fill, take, remaining, or exit.');
+    }
+
+}
