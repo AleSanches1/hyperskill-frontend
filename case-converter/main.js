b06ae3b547fb05d20d209c873f6e54ac63a32845
ale-sanches <130606847+ale-sanches@users.noreply.github.com>
2024-03-25 15:32:02 +0300

    case-converter: stage 3 completed

--- case-converter/main.js
+++ case-converter/main.js
@@ -13,6 +13,18 @@ function makeSentence(separator) {
     })
     textArea.value = arr.join(separator);
 }
+function download(filename, text) {
+    let element = document.createElement('a');
+    element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
+    element.setAttribute('download', filename);
+
+    element.style.display = 'none';
+    document.body.appendChild(element);
+
+    element.click();
+
+    document.body.removeChild(element);
+}
 
 const upperBtn = document.querySelector('#upper-case');
 upperBtn.addEventListener("click", function () {
@@ -34,3 +46,9 @@ sentenceBtn.addEventListener("click", function () {
     makeSentence(". ")
 })
 
+
+const saveBtn = document.querySelector("#save-text-file");
+saveBtn.addEventListener("click", function () {
+    download("text.txt", textArea.value);
+
+})
