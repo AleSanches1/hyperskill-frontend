e547302feb539db446ef26001fd5b26926346fdc
AleSanches1 <130606847+ale-sanches@users.noreply.github.com>
2024-04-01 12:49:07 +0300

    magic-form: stage 2/4 refactored

--- magic-form/main.js
+++ magic-form/main.js
@@ -8,30 +8,27 @@ document.addEventListener("DOMContentLoaded", function () {
         localStorage.setItem('formData', JSON.stringify(formData));
 
     })
-    let savedData = localStorage.getItem('formData');
-    let data = JSON.parse(savedData);
-    console.log(data)
+    const savedData = JSON.parse(localStorage.getItem('formData'));
 
     function fillInputs() {
-        let name = document.querySelector("#first-name");
-        name.value = data["first-name"];
-
-        let surname = document.querySelector("#last-name");
-        surname.value = data["last-name"];
-
-        let email = document.querySelector("#email");
-        email.value = data.email;
-
-        let phone = document.querySelector("#phone");
-        phone.value = data.phone;
-
-        let company = document.querySelector("#company");
-        company.value = data.company;
-
-        let address = document.querySelector("#address");
-        address.value = data.address;
+        const inputs = {
+            "first-name": "#first-name",
+            "last-name": "#last-name",
+            "email": "#email",
+            "phone": "#phone",
+            "company": "#company",
+            "address": "#address"
+        };
+        for (const key in inputs) {
+            if (inputs.hasOwnProperty(key)) {
+                const element = inputs[key]; //извлекаем значение свойства объекта inputs и сохраняем его в переменную element. Значение element представляет собой селектор элемента формы (например, "#first-name").
+                const input = document.querySelector(element); // используем document.querySelector с полученным селектором, чтобы найти соответствующий элемент формы. Найденный элемент сохраняется в переменной input.
+                input.value = savedData[key] || ""; //устанавливаем значение поля формы (input.value) равным значению, сохраненному в объекте savedData под ключом key. Если такого ключа нет в savedData, или если его значение равно null или undefined, то устанавливаем пустую строку в качестве значения поля формы.
+            }
+        }
     }
+
     fillInputs();
-   
+
 
 })
