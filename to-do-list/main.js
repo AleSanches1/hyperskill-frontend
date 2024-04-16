7ea9e7b2d622057a6d38b5ecd3c63c1b4c8410ef
AleSanches1 <130606847+ale-sanches@users.noreply.github.com>
2024-04-16 11:23:15 +0300

    to-do-list: stage 4

--- to-do-list/main.js
+++ to-do-list/main.js
@@ -1,40 +1,68 @@
 const inputField = document.querySelector("#input-task");
 const addTaskBtn = document.querySelector("#add-task-button");
-const taskList = document.querySelector("#task-list")
-const checkboxes = document.querySelectorAll(".checkbox")
+const taskList = document.querySelector("#task-list");
 
-addTaskBtn.addEventListener("click", function () {
-    const newTask = inputField.value.trim();
-    if (newTask !== "") {
-        createLiItem(newTask);
-        inputField.value = "";
+addTaskBtn.addEventListener("click", () => {
+    const task = inputField.value.trim();
+    createTask(task);
+    displayItems();
+    inputField.value = "";
+});
+
+
+let tasksArray = localStorage.getItem("tasks") ? JSON.parse(localStorage.getItem("tasks")) : [];
+console.log(tasksArray);
+
+function displayItems() {
+    let tasks = "";
+    for (let i = 0; i < tasksArray.length; i++) {
+        tasks += `  <li class="list-item">
+                    <div class="left-item"><input type="checkbox" class="checkbox"><span class="task">${tasksArray[i]}</span>
+                    </div>
+                    <button class="delete-btn">&times</button>
+                </li> `
     }
-});
-
-checkboxes.forEach(function (checkbox) {
-    checkbox.addEventListener("click", function () {
-        const parentDiv = checkbox.parentElement;
-        const taskSpan = parentDiv.querySelector(".task");
-        taskSpan.classList.toggle("checked", checkbox.checked);
-    });
-});
-
-
-function createLiItem(task) {
-    const listItem = document.createElement("li");
-    listItem.classList.add("list-item");
-
-    listItem.innerHTML = `
-        <div class="left-item">
-            <input class="checkbox" type="checkbox">
-            <span class="task">${task}</span>
-        </div>
-        <button class="delete-btn">&times;</button>
-    `;
-
-    listItem.querySelector('.delete-btn').addEventListener("click", function () {
-        listItem.remove();
-    });
-
-    taskList.appendChild(listItem);
+    taskList.innerHTML = tasks;
+    activateDeleteButton();
+    activateCheckbox();
 }
+
+function activateDeleteButton() {
+    const deleteBtn = document.querySelectorAll(".delete-btn");
+    deleteBtn.forEach((button, index) => {
+        button.addEventListener("click", () => {
+            deleteTask(index);
+        })
+    })
+}
+
+function deleteTask(index) {
+    tasksArray.splice(index, 1);
+    localStorage.setItem("tasks", JSON.stringify(tasksArray));
+    location.reload();
+}
+
+function activateCheckbox() {
+    const checkbox = document.querySelectorAll(".checkbox");
+    checkbox.forEach((box, index) => {
+        box.addEventListener("click", () => {
+            checkTask(index);
+        })
+    })
+}
+
+function checkTask(index) {
+    const taskElement = document.querySelectorAll(".task")[index];
+    const checkbox = document.querySelectorAll(".checkbox")[index];
+    taskElement.classList.toggle("checked", checkbox.checked);
+}
+
+function createTask(task) {
+    tasksArray.push(task)
+    localStorage.setItem("tasks", JSON.stringify(tasksArray))
+}
+
+window.onload = function () {
+    displayItems();
+}
+
