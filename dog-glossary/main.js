36cbb3756f534240597e06e33297833300d0f47d
AleSanches1 <130606847+ale-sanches@users.noreply.github.com>
2024-04-16 14:37:54 +0300

    dog-glossary: stage 2

--- dog-glossary/main.js
+++ dog-glossary/main.js
@@ -1,16 +1,44 @@
 window.addEventListener("DOMContentLoaded", function (evt) {
 
-    const showRandomDog = document.querySelector("#content");
-    const button = document.querySelector("#button-random-dog");
+    const content = document.querySelector("#content");
+    const randomButton = document.querySelector("#button-random-dog");
+    const showButton = document.querySelector("#button-show-breed");
 
-    button.addEventListener("click", async (evt)=>{
-        const imageURL = await getRandomImage();
-        showRandomDog.innerHTML = `<img src = "${imageURL}" alt = "random dog" />`;
-    });
 
-    const getRandomImage = async () => {
-        const response =await fetch ("https://dog.ceo/api/breeds/image/random");
+    document.addEventListener("click", async function (evt) {
+        if (evt.target === randomButton) {
+            const imageURL = await getRandomImage();
+            content.innerHTML = `<img src = "${imageURL}" alt = "random dog" />`;
+        }
+        if (evt.target === showButton) {
+            const response = await showBreed();
+            if(response){
+                content.innerHTML = `<img src = "${response}" alt = "dog" />`
+            }else{
+                content.innerHTML = `<p>Breed not found!</p>`
+            }
+
+        }
+    })
+
+    async function getRandomImage() {
+        const response = await fetch("https://dog.ceo/api/breeds/image/random");
         const data = await response.json();
         return data.message;
     }
+
+    async function showBreed() {
+        const breed = document.querySelector("#input-breed").value.toLowerCase();
+        const response = await fetch(`https://dog.ceo/api/breed/${breed}/images/random`);
+        if(!response.ok){
+            return false;
+        }
+        const data = await response.json();
+        return data.message;
+    }
+
+
+
+
 })
+
