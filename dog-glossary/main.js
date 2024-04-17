d744921411cdaa8e89f6fc4fcc9f11166490f914
AleSanches1 <130606847+ale-sanches@users.noreply.github.com>
2024-04-17 17:27:32 +0300

    dog-glossary: stage 3

--- dog-glossary/main.js
+++ dog-glossary/main.js
@@ -3,7 +3,7 @@ window.addEventListener("DOMContentLoaded", function (evt) {
     const content = document.querySelector("#content");
     const randomButton = document.querySelector("#button-random-dog");
     const showButton = document.querySelector("#button-show-breed");
-
+    const showSubBreedBtn = document.querySelector("#button-show-sub-breed");
 
     document.addEventListener("click", async function (evt) {
         if (evt.target === randomButton) {
@@ -11,13 +11,24 @@ window.addEventListener("DOMContentLoaded", function (evt) {
             content.innerHTML = `<img src = "${imageURL}" alt = "random dog" />`;
         }
         if (evt.target === showButton) {
-            const response = await showBreed();
-            if(response){
+            const response = await getInfoFromApi("images/random");
+            if (response) {
                 content.innerHTML = `<img src = "${response}" alt = "dog" />`
-            }else{
+            } else {
                 content.innerHTML = `<p>Breed not found!</p>`
             }
 
+        }
+        if (evt.target === showSubBreedBtn) {
+            // const response = await getInfoFromApi("images/random");
+            // if (!response) {
+            //     content.innerHTML = `<p>Breed not found!</p>`
+            // }
+            const breeds = await showSubBreedList();
+            console.log(breeds);
+            if (!breeds) {
+                content.innerHTML = `<p>No sub-breeds found!</p>`
+            }
         }
     })
 
@@ -27,13 +38,42 @@ window.addEventListener("DOMContentLoaded", function (evt) {
         return data.message;
     }
 
-    async function showBreed() {
+    // async function showBreed() {
+    //     const breed = document.querySelector("#input-breed").value.toLowerCase();
+    //     const response = await fetch(`https://dog.ceo/api/breed/${breed}/images/random`);
+    //     if (!response.ok) {
+    //         return false;
+    //     }
+    //     const data = await response.json();
+    //     return data.message;
+    // }
+
+    async function showSubBreedList() {
+
+        let result = await getInfoFromApi("list");
+        if (result) {
+            const ol = document.createElement("ol");
+            for (let value of result) {
+                const li = document.createElement("li");
+                li.innerHTML = value;
+                ol.appendChild(li);
+            }
+            content.innerHTML = "";
+            content.appendChild(ol);
+            return result;
+        }
+        return false;
+
+    }
+
+    async function getInfoFromApi(param) {
         const breed = document.querySelector("#input-breed").value.toLowerCase();
-        const response = await fetch(`https://dog.ceo/api/breed/${breed}/images/random`);
-        if(!response.ok){
+        const response = await fetch(`https://dog.ceo/api/breed/${breed}/${param}`);
+        if (!response.ok) {
             return false;
         }
         const data = await response.json();
         return data.message;
     }
 })
+
